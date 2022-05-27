9c78819c12c4c2a03f947912405175215a57ef3d
magicalbeauty91 <magicalbeauty91@gmail.com>
2022-05-27 12:57:33 -0500

    added inital structure to app.js

--- employee-tracker/app.js
+++ employee-tracker/app.js
@@ -3,10 +3,64 @@ const inquirer = require('inquirer');
 const workPrompt = () => {
     return inquirer.prompt([
         {
-
+            type: 'list',
+            name: 'workOption',
+            message: 'Choose an option',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
         }
     ])
+        .then(chosenOption => {
+            if (chosenOption.workOption === 'View all departments') {
+                return viewDepartments();
+            }
+            if (chosenOption.workOption === 'View all roles') {
+                return viewRoles();
+            }
+            if (chosenOption.workOption === 'View all employees') {
+                return viewEmployees();
+            }
+            if (chosenOption.workOption === 'Add a department') {
+                return addDepartment();
+            }
+            if (chosenOption.workOption === 'Add a role') {
+                return addRole();
+            }
+            if (chosenOption.workOption === 'Add an employee') {
+                return addEmployee();
+            }
+            if (chosenOption.workOption === 'Update an employee role') {
+                return updateEmployeeRole();
+            }
+    })
+};
+
+const viewDepartments = () => {
+
 }
 
+const viewRoles = () => {
 
+}
+
+const viewEmployees = () => {
+
+}
+
+const addDepartment = () => {
+
+}
+
+const addRole = () => {
+
+}
+
+const addEmployee = () => {
+
+}
+
+const updateEmployeeRole = () => {
+
+}
+
+workPrompt();
 
